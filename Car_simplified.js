adff28741d66cc5ec8c2a96eaecf7a45d162e4c3
Ivan Petrushev <ivanatora@gmail.com>
2016-09-22 13:55:42 +0300

    heading is now radians

--- Car_simplified.js
+++ Car_simplified.js
@@ -6,7 +6,7 @@ window.Car = function(genes){
     this.maximum_wheel_angle = 30;
     this.current_wheel_angle = 0;
     this.last_wheel_angle = 0;
-    this.heading = 0;
+    this.heading_radians = 0;
     this.last_heading = 0;
     this.delta_angle = 0;
     this.tr = 0;
@@ -40,18 +40,18 @@ window.Car = function(genes){
     var front_shape = new Path.Rectangle(0, 0, this.tire_w, this.tire_h);
     front_shape.fillColor = rgba(255, 0, 0, 0.5);
     front_shape.strokeColor = 'green';
-    front_shape.position.x = this.pos.x - axis_h/2 * cos(radians(this.heading));
-    front_shape.position.y = this.pos.y - axis_h/2 * sin(radians(this.heading));
-    front_shape.rotate(this.heading);
+    front_shape.position.x = this.pos.x - axis_h/2 * cos(this.heading_radians);
+    front_shape.position.y = this.pos.y - axis_h/2 * sin(this.heading_radians);
+    front_shape.rotate(degrees(this.heading_radians));
     front_shape.rotate(this.current_wheel_angle);
 
 
     var back_shape = new Path.Rectangle(0, 0, this.tire_w, this.tire_h);
     back_shape.fillColor = rgba(0, 0, 0, 0.1);
     back_shape.strokeColor = 'green';
-    back_shape.position.x = this.pos.x + axis_h/2 * cos(radians(this.heading));
-    back_shape.position.y = this.pos.y + axis_h/2 * sin(radians(this.heading));
-    back_shape.rotate(this.heading);
+    back_shape.position.x = this.pos.x + axis_h/2 * cos(this.heading_radians);
+    back_shape.position.y = this.pos.y + axis_h/2 * sin(this.heading_radians);
+    back_shape.rotate(degrees(this.heading_radians));
     
     
     tr_shape = new Path.Circle(new Point(0, 0), 1);
@@ -76,10 +76,10 @@ window.Car = function(genes){
         
         front_shape.rotate(-this.last_wheel_angle);
 //        this.last_heading = this.heading;
-        car_shape.rotate(-this.heading);
+        car_shape.rotate(degrees(-this.heading_radians));
         
-        front_shape.rotate(-this.heading);
-        back_shape.rotate(-this.heading);
+        front_shape.rotate(degrees(-this.heading_radians));
+        back_shape.rotate(degrees(-this.heading_radians));
         
         
         car_shape.position = this.pos;
@@ -102,7 +102,7 @@ window.Car = function(genes){
         // calculate turning radius and travelled arc length
         this.tr = axis_h / sin(radians(this.current_wheel_angle));
         turning_circle_len = abs(PI * 2 * this.tr);
-        arc_angle = 50 / this.tr; // 20 is default
+        arc_angle = 20 / this.tr; // 20 is default
 
         this.tr += car_w / 2;
         
@@ -124,20 +124,29 @@ window.Car = function(genes){
         console.log('cntr_to_back.angle', cntr_to_back.angle, 'arc_angle in degrees', degrees(arc_angle))
 
         new_arc_angle_radians = cntr_to_back.angleInRadians - arc_angle;
-//        new_arc_angle_radians =  arc_angle;
+        // new_arc_angle_radians = arc_angle;
+        console.log('new_arc_angle_radians = cntr_to_back.angleInRadians - arc_angle', new_arc_angle_radians, cntr_to_back.angleInRadians, arc_angle)
         if (this.current_wheel_angle < 0){
             new_arc_angle_radians = new_arc_angle_radians + PI;
         }
         else {
-//            new_arc_angle_radians = new_arc_angle_radians;
+           // new_arc_angle_radians = new_arc_angle_radians + PI/2;
         }
-        new_back = new Point(radius_center.x + this.tr * cos(new_arc_angle_radians), radius_center.y + this.tr * sin(new_arc_angle_radians));
+        console.log('finally new_arc_angle_radians', new_arc_angle_radians)
+        new_back = new Point(
+            radius_center.x + this.tr * cos(new_arc_angle_radians),
+            radius_center.y + this.tr * sin(new_arc_angle_radians));
         
-        this.heading += new_arc_angle_radians;
-        console.log('new heading', this.heading, 'newarc_angle in degrees', degrees(new_arc_angle_radians))
+        this.heading_radians += arc_angle;
+        console.log('new heading_radians', this.heading_radians, 'newarc_angle in degrees', degrees(new_arc_angle_radians))
         
-        new_pos = this.pos.clone().subtract(back_shape.position.clone().subtract(new_back));
-        new_pos.rotate(-degrees(new_arc_angle_radians), new_back);
+        // new_pos = this.pos.clone().subtract(back_shape.position.clone().subtract(new_back));
+        // new_pos.rotate(-degrees(new_arc_angle_radians), new_back);
+
+        new_pos = new Point(
+            new_back.x - axis_h/2 * cos(this.heading_radians),
+            new_back.y - axis_h/2 * sin(this.heading_radians))
+
         console.log('new_pos', new_pos)
         new_pos_shape.position = new_pos;
         
@@ -145,14 +154,14 @@ window.Car = function(genes){
 
         
         front_shape.rotate(this.current_wheel_angle);
-        car_shape.rotate(this.heading);
-//        car_shape.position = new_pos;
-        front_shape.position.x = car_shape.position.x - axis_h/2 * cos(radians(this.heading));
-        front_shape.position.y = car_shape.position.y - axis_h/2 * sin(radians(this.heading));
-        front_shape.rotate(this.heading);
-        back_shape.position.x = car_shape.position.x + axis_h/2 * cos(radians(this.heading));
-        back_shape.position.y = car_shape.position.y + axis_h/2 * sin(radians(this.heading));
-        back_shape.rotate(this.heading);
+        car_shape.rotate(degrees(this.heading_radians));
+        // car_shape.position = new_pos;
+        front_shape.position.x = car_shape.position.x - axis_h/2 * cos(this.heading_radians);
+        front_shape.position.y = car_shape.position.y - axis_h/2 * sin(this.heading_radians);
+        front_shape.rotate(degrees(this.heading_radians));
+        back_shape.position.x = car_shape.position.x + axis_h/2 * cos(this.heading_radians);
+        back_shape.position.y = car_shape.position.y + axis_h/2 * sin(this.heading_radians);
+        back_shape.rotate(degrees(this.heading_radians));
         
         this.update_counter++;
     }
--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 width = view.size.width;
 height = view.size.height;
-frame_every = 20;
+frame_every = 60;
 fps_counter = 0;
 
 iMutationRate = 1; //%
